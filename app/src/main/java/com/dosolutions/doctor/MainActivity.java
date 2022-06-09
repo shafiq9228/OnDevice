77e877903b742601564da35d7c677de2d3b38425
shafiqpatel <shafiqpatel@gmail.com>
2022-06-09 17:53:11 +0530

    Update MainActivity.java
    new yoast

--- app/src/main/java/com/dosolutions/doctor/MainActivity.java
+++ app/src/main/java/com/dosolutions/doctor/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello AndroBIm", Toast.LENGTH_SHORT).show();
     }
 }
